5d5efdfe7e439dd0371581bf78e3c5c863e492ad
YaroslavBoliachevets <92535542+YaroslavBoliachevets@users.noreply.github.com>
2022-07-21 02:10:46 +0000

    Deploying to gh-pages from @ YaroslavBoliachevets/filmoteka-copy@4edc12ed2bbfcdb05a8cd791925129981deaddac 🚀

---
+++ index.0e3b887c.js
@@ -0,0 +1,2 @@
+var mask=document.querySelector(".mask");window.addEventListener("load",(function(){mask.classList.add("done"),setTimeout((function(){mask.remove()}),1e3)}));
+//# sourceMappingURL=index.0e3b887c.js.map
